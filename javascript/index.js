3dff8027cfc8f512b845df568ff772b5ac05c186
philips-of-ng@philips <philipsedun@gmail.com>
2025-01-21 06:19:19 +0100

    jon belllion can sing

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,34 @@
-window.location.href = './auth/Welcome.html'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getFirestore, collection, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuQCpOAl39SwYbzIWzcKjepNcmilRpN2k",
+  authDomain: "waves-bank.firebaseapp.com",
+  projectId: "waves-bank",
+  storageBucket: "waves-bank.appspot.com",
+  messagingSenderId: "389165188354",
+  appId: "1:389165188354:web:f57be4686f20ef538cae1b",
+  measurementId: "G-XQZ14QK1W5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+
+// Collection reference for users
+const usersColRef = collection(db, "users");
+
+// ------------------------------
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = './auth/VerifyPin.html'
+  } else {
+    window.location.href = './auth/Welcome.html'
+  }
+})
 
